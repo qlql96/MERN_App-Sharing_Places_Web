5c2eab1991c219144039bce0cf2ee77d3c172710
ql <qilong555@gmail.com>
2022-12-16 14:09:08 +0800

    Add UserItem component

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList';
+
 const Users = () => {
-    return <h2>Users Works!</h2>;
+
+    const USERS = [
+        {
+            id: 'u1',
+            name: 'John',
+            image: 'https://images.freeimages.com/images/large-previews/2b1/labrador-retriever-1332487.jpg',
+            places: 3
+        }
+    ];
+
+    return <UsersList items={USERS} />;
 }
 
 export default Users;
