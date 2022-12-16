93ec46e6dad944ecc809d583e658e71b1c1c84f3
ql <qilong555@gmail.com>
2022-12-16 13:51:39 +0800

    Start setup, pages and routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,22 @@
-function App() {
-  
+import React from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+
+
+const App = () => {
+  return <Router>
+    <Switch>
+      <Route path="/">
+        <Users />
+      </Route>
+      <Route path="/places/new" exact>
+        <NewPlace />
+      </Route>
+      <Redirect to="/" />
+    </Switch>
+  </Router>
 }
 
 export default App;
