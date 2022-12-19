ff247918c06fb7d382788556dcdc7e931d4421a8
ql <qilong555@gmail.com>
2022-12-20 02:48:39 +0800

    Add getting users with a get request

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,19 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import UsersList from '../components/UsersList';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 
 const Users = () => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState();
+    const [loadedUsers, setLoadedUsers] = useState();
 
-    const USERS = [
-        {
-            id: 'u1',
-            name: 'John',
-            image: 'https://images.freeimages.com/images/large-previews/2b1/labrador-retriever-1332487.jpg',
-            places: 3
-        }
-    ];
+    useEffect(() => {
+        const sendRequest = async () => {
+            setIsLoading(true);
+            try {
+                const response = await fetch('http://localhost:5000/api/users');
+                const responseData = await response.json();
 
-    return <UsersList items={USERS} />;
+                if (!response.ok) {
+                    throw new Error(responseData.message);
+                }
+
+                setLoadedUsers(responseData.users);
+            } catch (err) {
+                setError(err.message);
+            }
+            setIsLoading(false);
+        };
+        sendRequest();
+    }, []);
+
+    const errorHandler = () => {
+        setError(null);
+    };
+
+    return (
+        <React.Fragment>
+            <ErrorModal error={error} onClear={errorHandler} />
+            {isLoading && (
+                <div className="center">
+                    <LoadingSpinner />
+                </div>
+            )}
+            {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+        </React.Fragment>
+    );
 }
 
 export default Users;
