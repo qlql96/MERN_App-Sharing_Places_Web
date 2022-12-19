93b291ceddf88f5e1d6a716f1ab6c20035e03f23
ql <qilong555@gmail.com>
2022-12-19 18:08:43 +0800

    Add the relation between places and users

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -34,7 +34,7 @@ const signup = async (req, res, next) => {
       new HttpError('Invalid inputs passed, please check your data.', 422)
     );
   }
-  const { name, email, password, places } = req.body;
+  const { name, email, password } = req.body;
 
   let existingUser
   try {
@@ -60,7 +60,7 @@ const signup = async (req, res, next) => {
     email,
     image: 'https://live.staticflickr.com/7631/26849088292_36fc52ee90_b.jpg',
     password,
-    places
+    places: []
   });
 
   try {
