99b157dbe9b1b6784e7c1f04ace55d2358de63bc
ql <qilong555@gmail.com>
2022-12-20 21:42:03 +0800

    Add authorization on delete places

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -199,6 +199,14 @@ const deletePlace = async (req, res, next) => {
     return next(error);
   }
 
+  if (place.creator.id !== req.userData.userId) {
+    const error = new HttpError(
+      'You are not allowed to delete this place.',
+      401
+    );
+    return next(error);
+  }
+
   try {
     const sess = await mongoose.startSession();
     sess.startTransaction();
--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -124,7 +124,7 @@ const login = async (req, res, next) => {
   if (!existingUser) {
     const error = new HttpError(
       'Invalid credentials, could not log you in.',
-      401
+      403
     );
     return next(error);
   }
@@ -143,7 +143,7 @@ const login = async (req, res, next) => {
   if (!isValidPassword) {
     const error = new HttpError(
       'Invalid credentials, could not log you in.',
-      401
+      403
     );
     return next(error);
   }
